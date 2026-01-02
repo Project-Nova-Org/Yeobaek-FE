9729039d683ab1f6f6dcbfc5fb96834c4dff4a3a
chaemin <cheamin0038@gmail.com>
2026-01-03 01:03:39 +0900

    [Feat] 공통 색상 추가

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,7 +2,9 @@ export const Colors = {
   black: "#000000",
   gray400: "#9CA3AF",
   white: "#FFFFFF",
+  background: "#F4F5F7",
 
   primary: "#1B2A41",
   inactive: "#9CA3AF",
+  disable: "#CECECE",
 };
