e4796f4e934ee2833e815053b4f9fb26fdaef9a2
chaemin <cheamin0038@gmail.com>
2026-01-02 22:35:09 +0900

    [Feat] FloatingMenu 컴포넌트 추가

---
+++ src/components/BottomTabBar/FloatingMenu.tsx
@@ -0,0 +1,53 @@
+import { View, Pressable, Animated, StyleSheet } from "react-native";
+import { useFloatingMenu } from "./useFloatingMenu";
+import { FloatingActionItem } from "./FloatingActionItem";
+import { barStyles } from "./BottomTabBar.styles";
+import { PlusIcon, OOTDIcon, ItemIcon, ClosetIcon } from "@/assets/icons";
+import { Colors } from "@/theme/colors";
+
+export function FloatingMenu() {
+  const { anim, isOpen, toggle, close, plusRotation } = useFloatingMenu();
+
+  return (
+    <>
+      {isOpen && <Pressable style={StyleSheet.absoluteFill} onPress={close} />}
+
+      <View style={barStyles.actionsLayer} pointerEvents="box-none">
+        <FloatingActionItem
+          anim={anim}
+          start={{ x: 120, y: 70 }}
+          mid={{ x: 55, y: -20 }}
+          end={{ x: 0, y: -66 }}
+        >
+          <ItemIcon color={Colors.white} />
+        </FloatingActionItem>
+
+        <FloatingActionItem
+          anim={anim}
+          start={{ x: 120, y: 70 }}
+          mid={{ x: 0, y: -66 }}
+          end={{ x: -78, y: -26 }}
+        >
+          <OOTDIcon color={Colors.white} />
+        </FloatingActionItem>
+
+        <FloatingActionItem
+          anim={anim}
+          start={{ x: 120, y: 70 }}
+          mid={{ x: 95, y: -5 }}
+          end={{ x: 78, y: -26 }}
+        >
+          <ClosetIcon color={Colors.white} />
+        </FloatingActionItem>
+      </View>
+
+      <Pressable onPress={toggle} style={barStyles.floatingButton}>
+        <View style={barStyles.floatingInner}>
+          <Animated.View style={{ transform: [{ rotate: plusRotation }] }}>
+            <PlusIcon color={Colors.primary} />
+          </Animated.View>
+        </View>
+      </Pressable>
+    </>
+  );
+}
